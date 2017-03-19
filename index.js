53fefedb327d542b3cb7f543316b654bdd43226b
Radon to <Radon to>
2017-03-19 14:14:10 +0100

    Test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ exports.handler = function(event, context, callback) {
 
 var handlers = {
      'LaunchRequest': function () {
-        this.emit('SagHalloNutzer');
+        this.emit('SagHalloNutzer'); 
     },
     'WoIstMeineBestellungIntent': function () {
         this.emit('MeineBestellungIst')
@@ -30,25 +30,25 @@ var handlers = {
         this.emit('KriegeIchErstattung')
     },
     'MeineBestellungIst' : function () {
-        this.emit(':ask', "Ich weiss nicht wo deine Bestellung ist", "Ich habe keine Ahnung wo deine Bestellung ist", "frag mich nicht")
+        this.emit(':ask', "Ich habe keine Ahnung wo deine Bestellung ist", "Hallo noch da?")
     },
     'SagHalloNutzer' : function () {
-        this.emit(':ask', "Hallo Nutzer, frage nach deiner Bestellung")
+        this.emit(':ask', "Hallo Nutzer, frage nach deiner Bestellung", "Hallo noch da?")
     },
     'LieferungWurdeVerzoegert' : function () {
-        this.emit(':ask', "Ich weiß nicht warum deine Bestellung verzögert wurde")
+        this.emit(':ask', "Ich weiß nicht warum deine Bestellung verzögert wurde", "Hallo noch da?")
     },
     'WasHabeIchBestellt' : function () {
-        this.emit(':ask', "Ich weiß nicht was du bestellt hast", "Streng dein eigenes Gehirn an")
+        this.emit(':ask', "Streng dein eigenes Gehirn an", "Hallo noch da?")
     },
     'KannEsSchneller' : function () {
-        this.emit(':ask', "Nein geduldige dich", "Geduld ist die Mutter aller Tugenden", "Wie wäre es mit abwarten und Tee trinken")
+        this.emit(':ask', "Wie wäre es mit abwarten und Tee trinken", "Hallo noch da?")
     },
     'KriegeIchErstattung' : function() {
-        this.emit(':ask', "Vielleicht", "Versuchs doch")
+        this.emit(':ask', "Vielleicht", "Versuchs doch", "Hallo noch da?")
     },
     'BestesTeam' : function () {
-        this.emit(':ask', "Jedes Team, in dem ich nicht zum Einsatz komme, ist es nicht einmal wert, erwähnt zu werden.")
+        this.emit(':ask', "Jedes Team, in dem ich nicht zum Einsatz komme, ist es nicht einmal wert, erwähnt zu werden.", "Hallo noch da?")
     },
     'WannKommtMeineBestellungIntent': function () {
         this.emit('MeineBestellungKommt')
@@ -60,13 +60,13 @@ var handlers = {
         this.emit('BestellungVersandbereitIn')
     },
     'MeineBestellungKommt' : function () {
-        this.emit(':ask', "Ich weiss nicht wann deine Bestellung kommt")
+        this.emit(':ask', "Ich weiss nicht wann deine Bestellung kommt", "Hallo noch da?")
     },
     'BestellungGefertigtIn': function () {
-        this.emit(':ask', "Sie ist gefertigt")
+        this.emit(':ask', "Sie ist gefertigt", "Hallo noch da?")
     },
     'BestellungVersandbereitIn': function () {
-        this.emit(':ask', "Sie ist bereits verschickt")
+        this.emit(':ask', "Sie ist bereits verschickt", "Hallo noch da?")
     },
     'StelleUnserTeamVorIntent': function () {
         this.emit('StelleTeamVor')
@@ -75,25 +75,25 @@ var handlers = {
         this.emit('StelleTeamIdeeVor')
    }, 
     'StelleTeamVor': function () {
-       this.emit(':ask', "Das Team das mich die letzten Tage Programmiert hat Nennt sich tork to träck es. besteht aus Winnie Julian Konrad Konstantin Timo und natürlich dem besten Coach von allen Thomas")
+       this.emit(':ask', "Das Team das mich die letzten Tage Programmiert hat Nennt sich tork to träck. es besteht aus Winnie Julian Konrad Konstantin Timo und natürlich dem besten Coach von allen Thomas", "Hallo noch da?")
     },
      'StelleTeamIdeeVor': function () {
-       this.emit(':ask', "das Thema der Idee ist Bestellungen in jeglicher größe zu Orten und Informationen über die Bestellung auf Nachfrage zu geben")
+       this.emit(':ask', "das Thema der Idee ist Bestellungen in jeglicher größe zu Orten und Informationen über die Bestellung auf Nachfrage zu geben", "Hallo noch da?")
     },
      'WarumBrauchtManUnsIntent': function () {
        this.emit('ManBrauchtUnsFuer')
     },
      'ManBrauchtUnsFuer': function () {
-       this.emit(':ask', "Ein Sprachgesteuertes Zuhause Oder ein Sprachgesteuerter Arbeitsplatz bietet viele Vorteile zum Beispiel wenn ein Mechaniker schmutzige Hände hat und dadurch keinen Laptop oder PC nutzen kann Bietet die Sprachsuche den Vorteil Zeit zu sparen und seine  Tastatur zu schonen")
+       this.emit(':ask', "Ein Sprachgesteuertes Zuhause Oder ein Sprachgesteuerter Arbeitsplatz bietet viele Vorteile zum Beispiel wenn ein Mechaniker schmutzige Hände hat und dadurch keinen Laptop oder PC nutzen kann Bietet die Sprachsuche den Vorteil Zeit zu sparen und seine  Tastatur zu schonen", "Hallo noch da?")
     },
      'WieFunktionierstDuIntent': function () {
         this.emit('IchFunktioniereDurch')
     }, 
     'IchFunktioniereDurch': function () {
-        this.emit(':ask', "tork to träck wurde mit Verschieden Technologien gebaut. Hinter mir steht die Amazon Cloud und Node jay es. Node jay es ist eine serverseitige Plattform in der Softwareentwicklung zum Betrieb von Netzwerkanwendungen. Insbesondere lassen sich Webserver damit realisieren. Node jay es wird in der JavaScript-Laufzeitumgebung „V8“ ausgeführt, die ursprünglich für Google Chrome entwickelt wurde, und bietet daher eine ressourcensparende Architektur, die eine besonders große Anzahl gleichzeitig bestehender Netzwerkverbindungen ermöglicht. Alexa ist eine Künstliche Intelligenz auf der letztendlich die app tork to träck läuft.")
+        this.emit(':ask', "tork to träck wurde mit Verschieden Technologien gebaut. Hinter mir steht die Amazon Cloud und Node jay es. Node jay es ist eine serverseitige Plattform in der Softwareentwicklung zum Betrieb von Netzwerkanwendungen. Insbesondere lassen sich Webserver damit realisieren. Node jay es wird in der JavaScript-Laufzeitumgebung „V8“ ausgeführt, die ursprünglich für Google Chrome entwickelt wurde, und bietet daher eine ressourcensparende Architektur, die eine besonders große Anzahl gleichzeitig bestehender Netzwerkverbindungen ermöglicht. Alexa ist eine Künstliche Intelligenz auf der letztendlich die app tork to träck läuft.", "Hallo noch da?")
     },
      'DankeIntent': function () {
-        this.emit('Danke')
+        this.emit("Danke. Applaus bitte in 3. 2. 1. Jetzt hahahahahaha", "Hallo noch da?")
     },
      'Danke': function () {
         this.emit(':tell', "gerne")
